9bfc52e8dd5cc37f1c2ec448deb23c41c5f74883
Manuel Roa <manuelroaojeda.java@gmail.com>
2024-09-15 19:54:17 -0300

    mostrando item

--- app/src/main/java/com/iacc/manuelroa_20240915/JugueteAdaptador.kt
+++ app/src/main/java/com/iacc/manuelroa_20240915/JugueteAdaptador.kt
@@ -1,4 +1,41 @@
 package com.iacc.manuelroa_20240915
 
-class JugueteAdaptador {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class JugueteAdaptador (
+    private var juguetes : List<Juguete>,
+    context: Context) : RecyclerView.Adapter<JugueteAdaptador.JugueteViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JugueteViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_juguete, parent, false)
+        return JugueteViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return juguetes.size
+    }
+
+    override fun onBindViewHolder(holder: JugueteViewHolder, position: Int) {
+        val juguete = juguetes[position]
+        holder.itemTitulo.text = juguete.titulo
+        holder.itemPrecio.text = juguete.precio.toString()
+    }
+
+    class JugueteViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+            val itemTitulo : TextView = itemView.findViewById(R.id.tvTituloJuguete)
+            val itemPrecio : TextView = itemView.findViewById(R.id.tvPrecioJuguete)
+    }
+
+    fun refreshList(newJuguete : List<Juguete>){
+        juguetes = newJuguete
+        notifyDataSetChanged()
+    }
+
+
+
 }
--- app/src/main/java/com/iacc/manuelroa_20240915/MainActivity.kt
+++ app/src/main/java/com/iacc/manuelroa_20240915/MainActivity.kt
@@ -7,20 +7,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.iacc.manuelroa_20240915.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
     private lateinit var db : JuguetesDataBaseHelper
+    private lateinit var jugueteAdaptador: JugueteAdaptador
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        db = JuguetesDataBaseHelper(this)
+        jugueteAdaptador = JugueteAdaptador(db.getAllJuguetes() , this)
+
+        binding.juguetesRv.layoutManager = LinearLayoutManager(this)
+        binding.juguetesRv.adapter = jugueteAdaptador
         binding.FABAgregarEntrada.setOnClickListener{
             startActivity(Intent(applicationContext  , AgregarJugueteActivity::class.java))
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        jugueteAdaptador.refreshList(db.getAllJuguetes())
+    }
+
 }
