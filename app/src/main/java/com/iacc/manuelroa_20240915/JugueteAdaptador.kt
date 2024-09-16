02040f89c7021da10343f7256ffcb1a338fead93
Manuel Roa <manuelroaojeda.java@gmail.com>
2024-09-16 11:11:52 -0300

    mostrando id

--- app/src/main/java/com/iacc/manuelroa_20240915/JugueteAdaptador.kt
+++ app/src/main/java/com/iacc/manuelroa_20240915/JugueteAdaptador.kt
@@ -1,10 +1,13 @@
 package com.iacc.manuelroa_20240915
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class JugueteAdaptador (
@@ -24,11 +27,25 @@ class JugueteAdaptador (
         val juguete = juguetes[position]
         holder.itemTitulo.text = juguete.titulo
         holder.itemPrecio.text = juguete.precio.toString()
+
+        holder.ivActualizarJuguete.setOnClickListener{
+            val intent = Intent(holder.itemView.context , ActualizarJugueteActivity::class.java).apply {
+                putExtra("id_juguete" , juguete.id)
+
+            }
+            holder.itemView.context.startActivity(intent)
+
+            Toast.makeText(
+                holder.itemView.context, "el juguete seleccionado es ${juguete.id}", Toast.LENGTH_SHORT
+            ).show()
+        }
+
     }
 
     class JugueteViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
             val itemTitulo : TextView = itemView.findViewById(R.id.tvTituloJuguete)
             val itemPrecio : TextView = itemView.findViewById(R.id.tvPrecioJuguete)
+            val ivActualizarJuguete : ImageView = itemView.findViewById(R.id.ivActualizarJuguete)
     }
 
     fun refreshList(newJuguete : List<Juguete>){
